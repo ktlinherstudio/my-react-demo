2cb7a539625d649c7d6842a707f9de1209d492d8
ktlinherstudio <catdogorz520@gmail.com>
2025-04-08 16:45:35 +0800

    0408-5

---
+++ src/App-0408-1.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1>Hi,React</h1>
+            <p>sfefeojfejgoerjgorefoergorereofrejfpw</p>
+        </>
+    )
+}
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App-0408-2.jsx'
+// import './index.css'
+import'./style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
