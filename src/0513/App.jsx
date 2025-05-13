0ffd88a930ba3ad92d840ba7d4457a2dcb199393
ktlinktlinherstudio <catdogorz520@gmail.com>
2025-05-13 14:07:13 +0800

    1324

--- src/0513/App.jsx
+++ src/0513/App.jsx
@@ -1,28 +1,29 @@
 export default function App(){
-    return(
-        <>
-        <h1>本地端存取資料-local storage</h1>
-        <button onClick={()=>{
-            window.localStorage.setItem('book1','html5');
-             window.localStorage.setItem('book2','css');
-              window.localStorage.setItem('book3','js');
-        }}>寫入（新增/修改）資料</button>
-        <button onClick={()=>{
-            // 已知key的寫法
-            //console.log(window.localStorage.getItem('book2'))
-            // 未知 請取全部資料
-            for(var i=0; i<window.localStorage.length;i++){
-                var key= window.localStorage.key(i);
-                console.log(window.localStorage.getItem(key));
+    let country=[
+            {
+                cityId:1,
+                cityName:"台北市",
+            },
+            {
+                cityId:2,
+                cityName:"桃園市",
             }
-        }}>讀取資料</button>
-        <button onClick={()=>{
-            window.localStorage.removeItem('book2');
-        }}>刪除資料</button>
-        <button onClick={()=>{
-            window.localStorage.clear();
-        }}>全部刪除</button>
-
-        </>
-    )
+        ]
+        return(
+            <>
+            <button onClick={()=>{
+                // 將陣列物件轉成字串
+                let countryString=JSON.stringify(country);
+                // 寫入字串到local storage
+                localStorage.setItem('countrys',countryString);
+            }}>寫入</button>
+            <button onClick={()=>{
+                // 取出local storage資料
+                let getData= localStorage.getItem('countrys');
+                // 將字串轉成陣列物件
+                let getDataArr=JSON.parse(getData);
+                console.log('getDataArr:',getDataArr,typeof(getDataArr));
+            }}>讀取</button>
+            </>
+        )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './0513/App.jsx'
+import App from './0513/App-Local-storage-single.jsx'
 // import './index.css'
 // import './style.css'
 import { BrowserRouter } from 'react-router-dom'
