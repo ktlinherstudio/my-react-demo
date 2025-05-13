b4f9d2028d90608c3e22e2dc54213a0f94f26eaf
ktlinktlinherstudio <catdogorz520@gmail.com>
2025-05-13 10:59:00 +0800

    0513-4

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './0512/App.jsx'
+import App from './0513/App.jsx'
 // import './index.css'
 // import './style.css'
 import { BrowserRouter } from 'react-router-dom'
