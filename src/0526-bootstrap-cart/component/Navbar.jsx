94de30fb959e580f0d347ee8faeaaf6ddcd55bb1
ktlinktlinherstudio <catdogorz520@gmail.com>
2025-10-30 16:50:09 +0800

    e39402-

--- src/0526-bootstrap-cart/component/Navbar.jsx
+++ src/0526-bootstrap-cart/component/Navbar.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react"
+import { CartContext } from "../store"
+
 export default function Navbar() {
+    const [state] = useContext(CartContext);
     return (
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="btn btn-outline-success position-relative" type="button">
                     購物車
-                    <span className="badge bg-danger position-absolute top-0 start-100 translate-middle">99</span>
+                    <span className="badge bg-danger position-absolute top-0 start-100 translate-middle">
+                        {state.cartList.length}
+                    </span>
                 </button>
             </div>
         </nav>
